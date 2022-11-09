95e64aa903c0a9273488524ee5618ebffe6b1928
Nikolaj Frey <lopu@lopusmbp13.local>
2022-11-09 20:52:26 +1100

    master Fixed gameboy screen size issue from container padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="w-full bg-green min-h-screen pb-16">
     <Nav />
     <div class="w-full flex items-center justify-center">
-      <div class="container px-4">
+      <div class="container px-2 md:px-4">
         <nuxt />
       </div>
     </div>
--- pages/game.vue
+++ pages/game.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="w-full flex items-center justify-center pt-12 pb-12">
-    <div class="container px-2">
-      <Game />
-    </div>
+    <Game />
   </div>
 </template>
