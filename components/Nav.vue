a23d356b6cbf98bbc97074e72f0aa4dd1c613243
Nikolaj Frey <lopu@lopusmbp13.local>
2022-11-09 14:09:54 +1100

    master

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex items-center justify-center text-white bg-grass">
-    <nuxt-link to="/" class="container font-bold px-2 py-3">
+    <nuxt-link to="/" class="container font-bold text-2xl px-2 py-5">
       The Pokémon World!
     </nuxt-link>
   </div>
